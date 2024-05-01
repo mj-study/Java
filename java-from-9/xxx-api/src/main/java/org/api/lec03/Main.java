89c22bd144118bb3f70d3bc1322435315c74571a
hitobi1014 <hitobi1014@gmail.com>
2024-05-01 23:40:31 +0900

    제네릭 타입의 변수 생성시 diamond 데이터타입 명시할 필요가없음

--- java-from-9/xxx-api/src/main/java/org/api/lec03/Main.java
+++ java-from-9/xxx-api/src/main/java/org/api/lec03/Main.java
@@ -18,10 +18,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		List<Integer> list1 = Arrays.asList(1, 2, 3);
-		List<Integer> list2 = Arrays.asList(4,5);
+		List<Integer> list2 = Arrays.asList(4, 5);
 		List<Integer> result = flatten(list1, list2);
 		System.out.println(result);
 
-
+		// 생성시 제네릭타입에 명시안해줘도 됨, diamond 데이터타입 명시 x (필수가아님)
+		InnerClass<Integer> innerclass = new InnerClass<>(3);
 	}
+
+	public static class InnerClass<T> {
+		private final T t;
+
+		public InnerClass(T t) {
+			this.t = t;
+		}
+	}
+
 }
