f1dc8fa2c615040e22f0866301f38c0daf677c28
hitobi1014 <hitobi1014@gmail.com>
2024-05-05 13:24:39 +0900

    Flow API  - Publisher  - Subscription  - Subscriber

---
+++ java-from-9/xxx-api/src/main/java/org/api/lec05/Main.java
@@ -0,0 +1,13 @@
+package org.api.lec05;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		CoffeeSubscriber subscriber = new CoffeeSubscriber();
+		CoffeePublisher publisher = new CoffeePublisher();
+
+		publisher.subscribe(subscriber);
+
+		Thread.sleep(5_000L);
+	}
+}
