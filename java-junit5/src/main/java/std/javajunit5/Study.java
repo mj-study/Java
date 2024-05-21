2499c41fca2cc6c43bd7fbb629637521acf978da
minjun <hitobi1014@gmail.com>
2024-05-21 16:03:22 +0900

    Junit5 - csv 테스트   - 데이터 직접 받기   - ArgumentAccessor 로 직접 받기   - 커스텀 Aggregator 로 클래스 직접 받기

--- java-junit5/src/main/java/std/javajunit5/Study.java
+++ java-junit5/src/main/java/std/javajunit5/Study.java
@@ -3,6 +3,12 @@ package std.javajunit5;
 public class Study {
 	private StudyStatus status;
 	private int limit;
+	private String name;
+
+	public Study(int limit, String name) {
+		this.limit = limit;
+		this.name = name;
+	}
 
 	public Study(int limit) {
 		if (limit < 0) {
@@ -12,11 +18,24 @@ public class Study {
 		this.limit = limit;
 	}
 
+	public String getName() {
+		return name;
+	}
+
 	public StudyStatus getStatus() {
 		return this.status;
 	}
 
 	public int getLimit() {
 		return limit;
+	}
+
+	@Override
+	public String toString() {
+		return "Study{" +
+			"status=" + status +
+			", limit=" + limit +
+			", name='" + name + '\'' +
+			'}';
 	}
 }
