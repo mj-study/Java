c45a27ed29e4fbab3f674da1444704d0d1138219
hitobi1014 <hitobi1014@gmail.com>
2024-09-24 14:36:11 +0900

    item26 로 타입은 사용하지 말라

---
+++ Effective-Java/src/main/java/com/example/effectivejava/section05/GenericExam.java
@@ -0,0 +1,39 @@
+package com.example.effectivejava.section05;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GenericExam {
+	public static void main(String[] args) {
+		// raw 타입은 안전하지 않음
+		List nonGeneric = new ArrayList();
+
+		nonGeneric.add("abc");
+		nonGeneric.add(123);
+
+		nonGeneric.forEach(System.out::println);
+
+		List<String> stringList = new ArrayList<>();
+		stringList.add("abcdef");
+		stringList.forEach(System.out::println);
+
+		Set<Object> set = new HashSet<>();
+		set.add(1);
+		set.add("문자");
+		set.add(true);
+		set.add(2.3f);
+
+		readOnly(set);
+	}
+
+	/**
+	 * set 읽기 전용 메서드
+	 * 비한정적 와일드카드를 사용했기에 해당 값을 다시 재연산에 사용x
+	 * @param s
+	 */
+	static void readOnly(Set<?> s) {
+		s.forEach(System.out::println);
+	}
+}
